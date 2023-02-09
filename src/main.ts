969779fd0b731c7a4b9afed1b416434ca39058dc
vtlua <vtlua@netcompany.com>
2023-02-09 13:11:20 +0700

    add logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { AppLogger } from "./logger/logger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: new AppLogger(),
+  });
   await app.listen(3000);
 }
 bootstrap();
